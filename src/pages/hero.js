a15aa9618970d70c58e7f6a74bafa60ab8eec529
Antoine Delcourte <adelcourte.pro@gmail.com>
2022-06-16 16:33:23 +0200

    Tryout : header circle

---
+++ src/pages/hero.js
@@ -0,0 +1,12 @@
+export default function Hero() {
+    return (
+        <section className="header">
+            <div className="header-shape">
+                <div className="header-shape-item"></div>
+            </div>
+            <div className="header-blend">
+                <h1 className="text-center text-120 font-700 uppercase">Un vigneron,<br/>un artiste,<br/>un vin unique.</h1>
+            </div>
+        </section>
+    )
+}
