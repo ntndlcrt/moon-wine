a9f6ed89f5f3ef59101ec27eef597e8a25ebcc74
Antoine Delcourte <adelcourte.pro@gmail.com>
2022-06-20 09:41:56 +0200

    Update tailwind config

--- src/context/Store.js
+++ src/context/Store.js
@@ -1,1 +1,113 @@
 import { createContext, useContext, useState, useEffect } from 'react'
+import { createShopifyCheckout, updateShopifyCheckout, setLocalData, saveLocalData } from 'context/utils'
+
+const CartContext = createContext()
+const AddToCartContext = createContext()
+const UpdateCartQuantityContext = createContext()
+
+export function useCartContext() {
+    return useContext(CartContext)
+}
+  
+export function useAddToCartContext() {
+    return useContext(AddToCartContext)
+}
+  
+export function useUpdateCartQuantityContext() {
+    return useContext(UpdateCartQuantityContext)
+}
+
+export function CartProvider({ children }) {
+    const [cart, setCart] = useState([])
+    const [checkoutId, setCheckoutId] = useState('')
+    const [checkoutUrl, setCheckoutUrl] = useState('')
+    const [isLoading, setisLoading] = useState(false)
+  
+    useEffect(() => {
+        setLocalData(setCart, setCheckoutId, setCheckoutUrl)
+    }, [])
+  
+    useEffect(() => {
+        const onReceiveMessage = (e) => {
+            console.log(e)
+            setLocalData(setCart, setCheckoutId, setCheckoutUrl)
+        }
+    
+        window.addEventListener("storage", onReceiveMessage)
+
+        return () => {
+            window.removeEventListener("storage", onReceiveMessage)
+        }
+    }, [])
+  
+    async function addToCart(newItem) {
+        setisLoading(true)
+
+        if (cart.length === 0) {
+            setCart([
+                ...cart,
+                newItem
+            ])
+    
+            const response = await createShopifyCheckout(newItem)
+            setCheckoutId(response.id)
+            setCheckoutUrl(response.webUrl)
+            saveLocalData(newItem, response.id, response.webUrl)
+    
+        } else {
+            let newCart = [...cart]
+            let itemAdded = false
+
+            newCart.map(item => {
+                if (item.variantId === newItem.variantId) {
+                    item.variantQuantity += newItem.variantQuantity
+                    itemAdded = true
+                }
+            })
+    
+            let newCartWithItem = [...newCart]
+            itemAdded ? '' : newCartWithItem = [...newCart, newItem]
+    
+            setCart(newCartWithItem)
+            await updateShopifyCheckout(newCartWithItem, checkoutId)
+            saveLocalData(newCartWithItem, checkoutId, checkoutUrl)
+        }
+
+        setisLoading(false)
+    }
+  
+    async function updateCartItemQuantity(id, quantity) {
+        setisLoading(true)
+
+        let newQuantity = Math.floor(quantity)
+
+        if (quantity === '') {
+            newQuantity = ''
+        }
+
+        let newCart = [...cart]
+
+        newCart.forEach(item => {
+            if (item.variantId === id) {
+                item.variantQuantity = newQuantity
+            }
+        })
+    
+        newCart = newCart.filter(i => i.variantQuantity !== 0)
+        setCart(newCart)
+    
+        await updateShopifyCheckout(newCart, checkoutId)
+        saveLocalData(newCart, checkoutId, checkoutUrl)
+        setisLoading(false)
+    }
+  
+    return (
+        <CartContext.Provider value={[cart, checkoutUrl, isLoading]}>
+            <AddToCartContext.Provider value={addToCart}>
+                <UpdateCartQuantityContext.Provider value={updateCartItemQuantity}>
+                    {children}
+                </UpdateCartQuantityContext.Provider>
+            </AddToCartContext.Provider>
+        </CartContext.Provider>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,9 @@
-// Will generate different spacing values ((0.8rem based))
-// const generateSpacing = (pixelsBase, values) => {
-//     return values.reduce((acc, v) => {
-//         acc[`${v.toString().replace(".", "_")}`] = `${((pixelsBase * v) * .1).toFixed(1)}rem`
-//         return acc;
-//     }, {})
-// }
-  
 const generateSpacing = (values) => {
     return values.reduce((acc, v) => {
         acc[`${v.toString().replace(".", "_")}`] = `${v}vw`
         return acc;
     }, {})
 }
-  
-  // Will generate font sizes values with "rem" as unit
-//   const generateFontSizes = (values) => {
-//     return values.reduce((acc, v) => {
-//         acc[v] = `${(v * .1).toFixed(1)}rem`;
-//         return acc;
-//     }, {})
-//   }
 
 const generateFontSizes = (values) => {
     return values.reduce((acc, v) => {
@@ -28,34 +12,30 @@ const generateFontSizes = (values) => {
     }, {})
 }
   
-  const generateValues = (values) => {
+const generateValues = (values) => {
     return values.reduce((acc, v) => {
         acc[`${v.toString().replace(".", "_")}`] = v;
         return acc;
     }, {})
-  }
-  
-  // Generate all kinds of percent classes from 1% to 150%
-  const generatePercent = () => {
+}
+
+const generatePercent = () => {
     let percents = {};
     for (let p = 0; p <= 150; p++) {
         percents[p] = `${p}%`
     }
     return percents;
-  }
+}
   
-  module.exports = {
-    // Change the ":" separator ((like in "md:w-full")) by "-" ((to have "md-w-full"))
-    //separator: "-",
+module.exports = {
     important: true,
     corePlugins: {
         container: false,
     },
     mode: 'jit',
-    // Allow to delete all unused styles from the final CSS file
-      content: [
-          './src/**/*.{js,jsx,ts,tsx}',
-      ],
+    content: [
+        './src/**/*.{js,jsx,ts,tsx}',
+    ],
     theme: {
         screens: {
             'sm': '640px',
@@ -91,19 +71,10 @@ const generateFontSizes = (values) => {
             'molgak': ['Molgak', 'serif'],
             'inter': ['Inter', 'sans-serif']
         },
-        //spacing: generateSpacing(8, [0, 0.25, 0.35, 0.5, 0.75, 1, 1.25, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6, 7, 7.5, 8, 9, 10, 12, 14, 16, 17.5, 18, 20, 22, 24, 26, 28, 30, 32, 40, 48, 55]),
         spacing: generateSpacing([0, 0.25, 0.35, 0.5, 0.55, 0.75, 0.8, 1, 1.1, 1.25, 1.5, 1.67, 2, 2.22, 2.5, 2.77, 3, 3.5, 4, 4.75, 5, 6, 6.25, 7, 7.5, 8, 9, 10, 12, 13, 14, 16, 16.8, 17.5, 18, 20, 22, 24, 26, 28, 30, 32, 40, 48, 55]),
         fontSize: generateFontSizes([1, 0.8, 2.5, 7.65, 12, 14, 15, 16, 18, 20, 24, 32, 36, 40, 48, 56, 72, 110, 120]),
         lineHeight: generateValues([1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 2]),
         fontWeight: generateValues([100, 200, 300, 400, 500, 600, 700, 800, 900]),
-        // inset: {
-        //     ...generatePercent(),
-        //     'auto': 'auto',
-        // },
-        // translate: {
-        //     ...generatePercent(),
-        //     'auto': 'auto',
-        // },
         width: {
             ...generatePercent(),
             'auto': 'auto',
@@ -119,4 +90,4 @@ const generateFontSizes = (values) => {
             content: 'fit-content',
         }
     }
-  }
+}
