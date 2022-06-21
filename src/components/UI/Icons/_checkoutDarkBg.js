514ad19daf08ef2c0148d61a9567bbe66ddeb206
Antoine Delcourte <adelcourte.pro@gmail.com>
2022-06-22 00:17:36 +0200

    stroke-width -> strokeWidth

--- src/components/UI/Icons/_checkoutDarkBg.js
+++ src/components/UI/Icons/_checkoutDarkBg.js
@@ -3,7 +3,7 @@ export default function CheckDarkBg() {
         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25">
             <g id="Groupe_63" data-name="Groupe 63" transform="translate(0.5 0.5)">
                 <path id="Tracé_13" data-name="Tracé 13" d="M0,0H24V24H0Z" fill="none"/>
-                <circle id="Ellipse_10" data-name="Ellipse 10" cx="12" cy="12" r="12" fill="none" stroke="rgba(245,244,236,0.2)" strokeLinecap="round" strokeLinejoin="round" stroke-width="1"/>
+                <circle id="Ellipse_10" data-name="Ellipse 10" cx="12" cy="12" r="12" fill="none" stroke="rgba(245,244,236,0.2)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1"/>
                 <path id="Tracé_14" data-name="Tracé 14" d="M9,12l2,2,4-4" fill="none" stroke="#f5f4ec" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"/>
             </g>
         </svg>
