07ceeb41501c20808bd3623654583b32a1b89e43
Antoine Delcourte <adelcourte.pro@gmail.com>
2022-06-22 01:18:37 +0200

    Updating HpGallery

--- src/components/sections/HpGallery/HpGallery.js
+++ src/components/sections/HpGallery/HpGallery.js
@@ -15,10 +15,16 @@ export default function HpGallery() {
     return (
         <section className="py-13_89 bg-gray_darkest" data-scroll-section>
             <div className="grid grid-cols-22 extraPadding items-center">
-                <div className="col-span-10">
-
+                <div className="col-span-9 h-full">
+                    <div className={styles.frame}>
+                        <div className={styles.frameBottle} data-scroll data-scroll-speed="4">
+                            <Image src="/img/png/bottles/oiseau-des-vignes.png" layout="fill" objectFit="contain" objectPosition="center" />
+                        </div>
+                        <Image src="/img/png/frame.png" layout="fill" objectFit="contain" objectPosition="center" />
+                        <div className={styles.frameBackground}></div>
+                    </div>
                 </div>
-                <div className="col-span-10 col-start-[12] text-beige">
+                <div className="col-span-10 col-start-[12] text-beige py-2">
                     <h3 className="title--xl mb-2_22">Un plaisir gustatif et visuel</h3>
                     <div className="mb-3_33">
                         <div className="flex items-center mb-1_1">
