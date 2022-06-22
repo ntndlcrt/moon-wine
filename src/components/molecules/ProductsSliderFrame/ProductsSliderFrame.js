b40c23371b523c5433d8091308e8f1cbfa38279b
Antoine Delcourte <adelcourte.pro@gmail.com>
2022-06-22 15:27:28 +0200

    Fixing ProductsSliderFrame

--- src/components/molecules/ProductsSliderFrame/ProductsSliderFrame.js
+++ src/components/molecules/ProductsSliderFrame/ProductsSliderFrame.js
@@ -9,7 +9,7 @@ import styles from './ProductsSliderFrame.module.scss'
 import 'swiper/css'
 
 export default function ProductsSliderFrame({  }) {
-    let [bottles, setBottles] = useState([])
+    let [bottles, setBottles] = useState(false)
 
     useEffect(() => {
         let bottlesSrcs = []
@@ -38,42 +38,23 @@ export default function ProductsSliderFrame({  }) {
             <div className={styles.frame}>
                 <Image src='/img/png/frame.png' layout="fill" object-fit="contain" />
             </div>
-            <Swiper
-                spaceBetween={290}
-                slidesPerView={'auto'}
-                autoplay={{delay: 3000}}
-                loop={true}
-                className={styles.swiper}
-                modules={[Autoplay]}
-            >
-                <SwiperSlide>
-                    <img src={bottles[0]} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={bottles[1]} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={bottles[2]} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={bottles[3]} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={bottles[4]} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={bottles[5]} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={bottles[6]} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={bottles[7]} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={bottles[8]} alt="" />
-                </SwiperSlide>
-            </Swiper>
+            {bottles &&
+                <Swiper
+                    slidesPerView={'auto'}
+                    autoplay={{delay: 3000}}
+                    loop={true}
+                    className={styles.swiper}
+                    modules={[Autoplay]}
+                >
+                    {bottles.map(bottle => {
+                        return (
+                            <SwiperSlide>
+                                <Image src={bottle} layout="fill" objectFit="contain" objectPosition="center" />
+                            </SwiperSlide>
+                        )
+                    })}
+                </Swiper>
+            }
             <div className={styles.framePattern}>
                 <PatternFrame />
             </div>
