3c7029d5d2c1c69c852e2eada5054f2b27a471ec
Antoine Delcourte <adelcourte.pro@gmail.com>
2022-06-22 16:55:20 +0200

    Updating galery

--- src/pages/galerie.js
+++ src/pages/galerie.js
@@ -10,27 +10,27 @@ export default function Galery({ columns, width, top, left }) {
     const constraintsRef = useRef(null)
 
     var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-myHeaders.append("X-Shopify-Storefront-Access-Token", "264ef0e372d2093b9a8ad4ab597725cd");
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("X-Shopify-Storefront-Access-Token", "264ef0e372d2093b9a8ad4ab597725cd");
 
-var graphql = JSON.stringify({
-  query: "{\n	products(first: 2) {\n		edges {\n      node {\n        metafields(first:15) {\n          edges {\n            node {\n              id\n              namespace\n              value\n            }\n          }\n        }\n      }\n    }\n	}\n}",
-  variables: {}
-})
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: graphql,
-  redirect: 'follow'
-};
+    var graphql = JSON.stringify({
+    query: "{\n	products(first: 2) {\n		edges {\n      node {\n        metafields(first:15) {\n          edges {\n            node {\n              id\n              namespace\n              value\n            }\n          }\n        }\n      }\n    }\n	}\n}",
+    variables: {}
+    })
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: graphql,
+    redirect: 'follow'
+    };
 
-fetch("https://moon-wine.myshopify.com/api/2021-07/graphql.json", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error))
+    fetch("https://moon-wine.myshopify.com/api/2021-07/graphql.json", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error))
 
     return (
-        <motion.div className="galery" ref={constraintsRef}>
+        <motion.div className="galery" ref={constraintsRef} data-scroll-section>
             <motion.div
                 className="galery__cards"
                 style={{
